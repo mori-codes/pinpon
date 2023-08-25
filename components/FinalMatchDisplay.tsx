7380805ad9e412c9a6e2ae6d0566f398549ec95e
mori-codes <mori.codes.oficial@gmail.com>
2023-08-26 00:52:52 +0200

    Final touches

--- components/FinalMatchDisplay.tsx
+++ components/FinalMatchDisplay.tsx
@@ -1,28 +1,37 @@
-import { Match, Player } from "../server/types.ts";
+import { Match, Player } from "../server/types.ts"
 
 type ExtendedFinalMatch = Omit<Match, "player1" | "player2"> & {
-  player1?: Player;
-  player2?: Player;
-};
+  player1?: Player
+  player2?: Player
+}
 
 type Props = {
-  match: ExtendedFinalMatch;
-};
+  match: ExtendedFinalMatch
+}
 
 const FinalMatchDisplay = ({ match }: Props) => {
+  const score = match.score ?? [0, 0]
   return (
     <a className="w-64" href={`/update/${match.index}`}>
-      <div className="border-1 w-full flex justify-between py-1 px-2 ">
+      <div
+        className={`border-1 w-full flex justify-between py-1 px-2 ${
+          score[0] > score[1] ? "font-bold" : ""
+        }`}
+      >
         <span>{match.player1?.name ?? "-"}</span>
         <span>{match.score?.[0] ?? "0"}</span>
       </div>
-      <div className="border-1 w-full flex justify-between py-1 px-2 bg-gray-100">
+      <div
+        className={`border-1 w-full flex justify-between py-1 px-2 bg-gray-100 ${
+          score[0] < score[1] ? "font-bold" : ""
+        }`}
+      >
         <span>{match.player2?.name ?? "-"}</span>
         <span>{match.score?.[1] ?? "0"}</span>
       </div>
     </a>
-  );
-};
+  )
+}
 
-export { FinalMatchDisplay };
-export type { ExtendedFinalMatch };
+export { FinalMatchDisplay }
+export type { ExtendedFinalMatch }
--- routes/championship.tsx
+++ routes/championship.tsx
@@ -1,40 +1,59 @@
 import {
   ExtendedFinalMatch,
   FinalMatchDisplay,
-} from "../components/FinalMatchDisplay.tsx";
-import { fetchAllFinalMatches } from "../server/fetchAllFinalMatches.ts";
-import { fetchAllPlayers } from "../server/fetchAllPlayers.ts";
+} from "../components/FinalMatchDisplay.tsx"
+import { fetchAllFinalMatches } from "../server/fetchAllFinalMatches.ts"
+import { fetchAllPlayers } from "../server/fetchAllPlayers.ts"
 
 type Props = {
-  matches: Array<ExtendedFinalMatch>;
-  title: string;
-};
+  matches: Array<ExtendedFinalMatch>
+  title: string
+  final?: boolean
+}
 
-const ChampionshipSlide = ({ matches, title }: Props) => {
+const ChampionshipSlide = ({ matches, title, final }: Props) => {
   return (
     <div
-      className="w-screen flex-grow flex-shrink-0 flex flex-col items-center"
+      className="w-screen flex-grow flex-shrink-0 flex flex-col items-center relative"
       style={{
         scrollSnapAlign: "start",
       }}
     >
       <h2 className="text-xl">{title}</h2>
       <div className="flex flex-col flex-grow justify-around">
-        {matches.map((match) => <FinalMatchDisplay match={match} />)}
+        {final ? (
+          <>
+            <FinalMatchDisplay match={matches[0]} />
+            <div className="flex flex-col items-center gap-2">
+              <h2 className="text-lg">Tercer Puesto</h2>
+              <div className="flex" style={{ transform: "scale(80%)" }}>
+                <FinalMatchDisplay match={matches[1]} />
+              </div>
+            </div>
+          </>
+        ) : (
+          matches.map((match) => <FinalMatchDisplay match={match} />)
+        )}
+      </div>
+      <div
+        className="absolute top-1/2 right-full pr-2"
+        style={{ filter: "hue-rotate(320deg)" }}
+      >
+        ➡️
       </div>
     </div>
-  );
-};
+  )
+}
 
 const Championship = async () => {
-  const matches = await fetchAllFinalMatches();
-  const players = await fetchAllPlayers();
+  const matches = await fetchAllFinalMatches()
+  const players = await fetchAllPlayers()
 
   const extendedMatches = matches.map((match) => ({
     ...match,
     player1: players.find((player) => player._id === match.player1),
     player2: players.find((player) => player._id === match.player2),
-  }));
+  }))
 
   return (
     <div
@@ -51,9 +70,13 @@ const Championship = async () => {
         matches={extendedMatches.slice(4, 6)}
         title="Semifinal"
       />
-      <ChampionshipSlide matches={extendedMatches.slice(6)} title="Final" />
+      <ChampionshipSlide
+        matches={extendedMatches.slice(6)}
+        title="Final"
+        final
+      />
     </div>
-  );
-};
+  )
+}
 
-export default Championship;
+export default Championship
